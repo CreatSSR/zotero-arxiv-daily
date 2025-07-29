741d61e39b572e7d22580cc0b35c1e072c5ddce4
OvRisi <2095788297@qq.com>
2025-07-29 15:25:52 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -17,6 +17,7 @@ class ArxivPaper:
     def __init__(self,paper:arxiv.Result):
         self._paper = paper
         self.score = None
+        self.tag = tag  # 添加 tag 属性
     
     @property
     def title(self) -> str:
